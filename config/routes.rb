abfcb3934d617112a52cc8304eaf17c3c2895dd2
daichi_sato <daichiexperience@gmail.com>
2022-04-07 08:36:46 +0900

    model fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      resources :players
-      resources :games, only: %i(index)
+      resources :microposts, only: %i[index create show update destroy]
       namespace 'auth' do
         post 'users' => 'users#create'
       end
