e37cf7654dfff34b1dcb5c6b0ec6d96aea49784c
daichi_sato <daichiexperience@gmail.com>
2022-04-07 08:14:22 +0900

    [add] micropost model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_28_114803) do
+ActiveRecord::Schema.define(version: 2022_04_06_231248) do
+
+  create_table "microposts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.text "content", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", default: "", null: false
@@ -20,4 +28,5 @@ ActiveRecord::Schema.define(version: 2022_03_28_114803) do
     t.index ["uid"], name: "index_users_on_uid", unique: true
   end
 
+  add_foreign_key "microposts", "users"
 end
