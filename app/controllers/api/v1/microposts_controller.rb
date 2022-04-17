42afe6b6c9ec5ebdaded62121e3dc9449721acf4
daichi_sato <daichiexperience@gmail.com>
2022-04-17 15:03:20 +0900

    [add] 基本的なcrud完了

--- app/controllers/api/v1/microposts_controller.rb
+++ app/controllers/api/v1/microposts_controller.rb
@@ -8,25 +8,24 @@ class Api::V1::MicropostsController < ApplicationController
   end
 
   def create
-    @user = current_user
     @micropost = current_user.microposts.create!(micropost_params)
     render :template => "api/v1/microposts/show.json.jb"
   end
 
   def show
-    micropost = Micropost.find(params[:id])
+    @micropost = Micropost.find(params[:id])
     render :template => "api/v1/microposts/show.json.jb"
   end
 
   def update
-    micropost = current_user.microposts.find(params[:id])
-    micropost.update!(micropost_params)
+    @micropost = current_user.microposts.find(params[:id])
+    @micropost.update!(micropost_params)
     render :template => "api/v1/microposts/show.json.jb"
   end
 
   def destroy
-    micropost = current_user.microposts.find(params[:id])
-    micropost.destroy!
+    @micropost = current_user.microposts.find(params[:id])
+    @micropost.destroy!
     render :template => "api/v1/microposts/show.json.jb"
   end
 
